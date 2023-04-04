829e4b3b1668c0024825c235228848777acb1578
Tanmay <www.tanmayjauhari@gmail.com>
2023-04-04 18:49:07 +0530

    react hook form

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,32 +1,25 @@
-'use client'
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Contact = () => {
-  const [formFields, setFormFields] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormFields((prevState) => ({ ...prevState, [name]: value }));
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
-    <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <label htmlFor="name" style={{ marginBottom: '10px' }}>Name</label>
-      <input type="text" name="name" id="name" value={formFields.name} onChange={handleInputChange} style={{ marginBottom: '20px', padding: '5px' }} />
+      <input type="text" name="name" id="name" {...register('name', { required: true })} style={{ marginBottom: '20px', padding: '5px' }} />
+      {errors.name && <span style={{ color: 'red' }}>Name is required</span>}
 
       <label htmlFor="email" style={{ marginBottom: '10px' }}>Email</label>
-      <input type="email" name="email" id="email" value={formFields.email} onChange={handleInputChange} style={{ marginBottom: '20px', padding: '5px' }} />
+      <input type="email" name="email" id="email" {...register('email', { required: true, pattern: /^\S+@\S+$/i })} style={{ marginBottom: '20px', padding: '5px' }} />
+      {errors.email && <span style={{ color: 'red' }}>Please enter a valid email address</span>}
 
       <label htmlFor="message" style={{ marginBottom: '10px' }}>Message</label>
-      <textarea name="message" id="message" value={formFields.message} onChange={handleInputChange} style={{ marginBottom: '20px', padding: '5px' }} />
+      <textarea name="message" id="message" {...register('message', { required: true })} style={{ marginBottom: '20px', padding: '5px' }} />
+      {errors.message && <span style={{ color: 'red' }}>Message is required</span>}
 
       <button type="submit" style={{ backgroundColor: 'blue', color: 'white', padding: '10px', borderRadius: '5px' }}>Send</button>
     </form>
@@ -34,3 +27,4 @@ const Contact = () => {
 };
 
 export default Contact;
+
