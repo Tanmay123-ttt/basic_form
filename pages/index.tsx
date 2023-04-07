31f54a8afba23b81c01b9906be1d5c1fd4a2cfa9
Tanmay <www.tanmayjauhari@gmail.com>
2023-04-07 19:53:17 +0530

    class-component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { Component } from "react";
 import { HiMenu } from "react-icons/hi";
 import { AiOutlineDelete } from "react-icons/ai";
 import { MdCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md";
@@ -9,116 +9,157 @@ interface TodoItem {
   completed: boolean;
 }
 
-const Todo = () => {
-  const [todoItems, setTodoItems] = useState<TodoItem[]>([]);
-  const [newTodo, setNewTodo] = useState("");
-  const [menuOpen, setMenuOpen] = useState(false);
+interface TodoState {
+  todoItems: TodoItem[];
+  newTodo: string;
+  menuOpen: boolean;
+}
 
-  const handleNewTodoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewTodo(event.target.value);
-  };
+class Todo extends Component<{}, TodoState> {
+  constructor(props: {}) {
+    super(props);
 
-  const handleNewTodoSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    this.state = {
+      todoItems: [],
+      newTodo: "",
+      menuOpen: false,
+    };
+
+    this.handleNewTodoChange = this.handleNewTodoChange.bind(this);
+    this.handleNewTodoSubmit = this.handleNewTodoSubmit.bind(this);
+    this.handleTodoItemDelete = this.handleTodoItemDelete.bind(this);
+    this.handleTodoItemCompleteToggle =
+      this.handleTodoItemCompleteToggle.bind(this);
+    this.handleAddButtonClick = this.handleAddButtonClick.bind(this);
+  }
+
+  handleNewTodoChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ newTodo: event.target.value });
+  }
+
+  handleNewTodoSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    if (newTodo.trim() !== "") {
+    this.addNewTodoItem();
+  }
+
+  handleAddButtonClick() {
+    this.addNewTodoItem();
+  }
+
+  addNewTodoItem() {
+    if (this.state.newTodo.trim() !== "") {
       const newId =
-        todoItems.length > 0 ? todoItems[todoItems.length - 1].id + 1 : 1;
+        this.state.todoItems.length > 0
+          ? this.state.todoItems[this.state.todoItems.length - 1].id + 1
+          : 1;
       const newTodoItem: TodoItem = {
         id: newId,
-        text: newTodo.trim(),
+        text: this.state.newTodo.trim(),
         completed: false,
       };
-      setTodoItems([...todoItems, newTodoItem]);
-      setNewTodo("");
+      this.setState((prevState) => ({
+        todoItems: [...prevState.todoItems, newTodoItem],
+        newTodo: "",
+      }));
     }
-  };
+  }
 
-  const handleTodoItemDelete = (itemId: number) => {
-    setTodoItems(todoItems.filter((item) => item.id !== itemId));
-  };
+  handleTodoItemDelete(itemId: number) {
+    this.setState((prevState) => ({
+      todoItems: prevState.todoItems.filter((item) => item.id !== itemId),
+    }));
+  }
 
-  const handleTodoItemCompleteToggle = (itemId: number) => {
-    setTodoItems(
-      todoItems.map((item) => {
+  handleTodoItemCompleteToggle(itemId: number) {
+    this.setState((prevState) => ({
+      todoItems: prevState.todoItems.map((item) => {
         if (item.id === itemId) {
           return { ...item, completed: !item.completed };
         }
         return item;
-      })
-    );
-  };
+      }),
+    }));
+  }
 
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="w-full max-w-lg bg-white rounded-lg shadow-lg overflow-hidden">
-        <div className="flex justify-between items-center bg-pink-500 py-4 px-6">
-          <h1 className="text-white font-bold text-xl">Todo</h1>
-          <button
-            className="text-white focus:outline-none focus:ring-2 focus:ring-pink-600 rounded-md p-2"
-            onClick={() => setMenuOpen(!menuOpen)}
-          >
-            <HiMenu size={24} />
-          </button>
-        </div>
-        <div className="p-6">
-          <form
-            onSubmit={handleNewTodoSubmit}
-            className="flex flex-row items-center mb-6"
-          >
-            <input
-              type="text"
-              placeholder="Add a new todo..."
-              value={newTodo}
-              onChange={handleNewTodoChange}
-              className="flex-1 p-4 rounded-l-lg border border-gray-300 text-gray-800 focus:outline-none focus:ring-2 focus:ring-pink-600 focus:border-transparent"
-            />
-          </form>
-          <ul>
-            {todoItems.map((item) => (
-              <li
-                key={item.id}
-                className={`flex flex-row justify-between items-center bg-gray-200 p-6 mb-4 rounded-lg shadow-md ${
-                  item.completed ? "line-through" : ""
-                }`}
-              >
-                <div
-                  className="flex items
-
--center"
-                >
-                  <button
-                    className="text-gray-600 focus:outline-none focus:ring-2 focus:ring-pink-600 rounded-md p-2"
-                    onClick={() => handleTodoItemCompleteToggle(item.id)}
-                  >
-                    {item.completed ? (
-                      <MdCheckBox size={24} />
-                    ) : (
-                      <MdCheckBoxOutlineBlank size={24} />
-                    )}
-                  </button>
-                  <p className="ml-4">{item.text}</p>
-                </div>
-                <button
-                  className="text-gray-600 focus:outline-none focus:ring-2 focus:ring-pink-600 rounded-md p-2"
-                  onClick={() => handleTodoItemDelete(item.id)}
-                >
-                  <AiOutlineDelete size={24} />
-                </button>
-              </li>
-            ))}
-          </ul>
-          <div className="bg-white py-4 px-6 flex justify-center">
+  render() {
+    return (
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        <div className="w-full max-w-lg bg-white rounded-lg shadow-lg overflow-hidden">
+          <div className="flex justify-between items-center bg-pink-500 py-4 px-6">
+            <h1 className="text-white font-bold text-xl">Todo</h1>
             <button
-              onClick={handleNewTodoSubmit}
-              className="text-white bg-pink-500 rounded-full p-4 focus:outline-none focus:ring-2 focus:ring-pink-600"
+              className="text-white focus:outline-none focus:ring-2 focus:ring-pink-600 rounded-md p-2"
+              onClick={() =>
+                this.setState((prevState) => ({
+                  menuOpen: !prevState.menuOpen,
+                }))
+              }
+            >
+              <HiMenu size={24} />
+            </button>
+          </div>
+          <div className="p-6">
+            <form
+              onSubmit={this.handleNewTodoSubmit}
+              className="flex flex-row items-center mb-6"
+            >
+              <input
+                type="text"
+                placeholder="Add a new todo"
+                value={this.state.newTodo}
+                onChange={this.handleNewTodoChange}
+                className="flex-grow mr-4 py-2 px-4 rounded-md
+                bg-gray-200 text-gray-700 focus:outline-none focus:bg-white"
+              />
+            </form>
+            <div className="space-y-2">
+              {this.state.todoItems.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex items-center justify-between bg-gray-200 rounded-md py-2 px-4"
+                >
+                  <div
+                    className="flex items-center"
+                    style={
+                      item.completed
+                        ? { textDecoration: "line-through", color: "gray" }
+                        : {}
+                    }
+                  >
+                    <button
+                      className="text-gray-500 hover:text-pink-500 focus:outline-none focus:ring-2 focus:ring-pink-600 rounded-md p-1"
+                      onClick={() => this.handleTodoItemCompleteToggle(item.id)}
+                    >
+                      {item.completed ? (
+                        <MdCheckBox size={24} />
+                      ) : (
+                        <MdCheckBoxOutlineBlank size={24} />
+                      )}
+                    </button>
+                    <span className="ml-2">{item.text}</span>
+                  </div>
+                  <button
+                    className="text-gray-500 hover:text-red-500 focus:outline-none focus:ring-2 focus:ring-red-600 rounded-md p-1"
+                    onClick={() => this.handleTodoItemDelete(item.id)}
+                  >
+                    <AiOutlineDelete size={24} />
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex justify-center items-center  py-4">
+            <button
+              className="bg-pink-500 hover:bg-pink-400 text-white py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-pink-600"
+              onClick={this.handleAddButtonClick}
             >
               Add
             </button>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Todo;
